083d271c080f6b4dc75f5dfe52fc525e72a0ca30
pantyetta <62027884+pantyetta@users.noreply.github.com>
2022-03-02 00:15:34 +0900

    change gradient and add js

---
+++ src/main.js
@@ -0,0 +1,4 @@
+﻿$(window).on('mousemove', function (e){
+   $('#bg').css('top', ($(window).height()/2 - e.clientY) / 100 - 10 + "%");
+   $(`#bg`).css('left', ($(window).width()/2 - e.clientX) / 100 - 10 + "%");
+});
